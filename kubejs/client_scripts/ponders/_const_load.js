79f959f4022bee43908ab2374a67acc06f2136a7
Softest Cat in Town <50622547+ReeCodes@users.noreply.github.com>
2025-11-03 15:31:58 +0100

    1.20.1_v0.2

--- kubejs/client_scripts/ponders/_const_load.js
+++ kubejs/client_scripts/ponders/_const_load.js
@@ -1,10 +1,9 @@
-/*
-	Embers Rekindled x Aetherworks - Ponders by 'RQT
-*/
-
 const $AABB = Java.loadClass("net.minecraft.world.phys.AABB");
+//EMBERS
+const $PrismBlockEntity = Java.loadClass("net.sirplop.aetherworks.blockentity.PrismBlockEntity");
+const $AWUtils = Java.loadClass("net.sirplop.aetherworks.util.Utils");
+const $EmbersColors = Java.loadClass("com.rekindled.embers.util.EmbersColors");
+
 const $GlowParticleOptions = Java.loadClass("com.rekindled.embers.particle.GlowParticleOptions");
 const $SmokeParticleOptions = Java.loadClass("com.rekindled.embers.particle.SmokeParticleOptions");
 const $SparkParticleOptions = Java.loadClass("com.rekindled.embers.particle.SparkParticleOptions");
-const $CreateEmitter = Java.loadClass("com.simibubi.create.foundation.ponder.instruction.EmitParticlesInstruction");
-const $AWUtils = Java.loadClass("net.sirplop.aetherworks.util.Utils");
